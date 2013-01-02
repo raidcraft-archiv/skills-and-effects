cb16adf84f3e7ae26e355bdc42d7e55081b93f09
Silthus <silthus@raid-craft.de>
2013-01-02 12:50:34 +0100

    armor skill now uses a formula to calculate the damage reduction based on armor points

--- src/main/java/de/raidcraft/skillsandeffects/skills/armor/Armor.java
+++ src/main/java/de/raidcraft/skillsandeffects/skills/armor/Armor.java
@@ -3,12 +3,12 @@ package de.raidcraft.skillsandeffects.skills.armor;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.skills.api.combat.AttackSource;
 import de.raidcraft.skills.api.combat.AttackType;
+import de.raidcraft.skills.api.combat.action.Attack;
 import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.level.Levelable;
 import de.raidcraft.skills.api.persistance.SkillProperties;
 import de.raidcraft.skills.api.profession.Profession;
 import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
-import de.raidcraft.skills.api.skill.Skill;
 import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.TriggerHandler;
 import de.raidcraft.skills.api.trigger.Triggered;
@@ -16,6 +16,7 @@ import de.raidcraft.skills.tables.THeroSkill;
 import de.raidcraft.skills.trigger.DamageTrigger;
 import de.raidcraft.skills.trigger.InventoryClickTrigger;
 import de.raidcraft.skills.trigger.InventoryCloseTrigger;
+import de.raidcraft.skills.trigger.ItemHeldTrigger;
 import de.raidcraft.skills.util.ItemUtil;
 import de.raidcraft.util.ItemUtils;
 import org.bukkit.ChatColor;
@@ -28,6 +29,12 @@ import java.util.EnumMap;
 import java.util.Map;
 
 /**
+ * Each item can have a configured armor value which are just points. The factor these points
+ * are are converted into damage recution can be defined here. The points need to be given each
+ * item in the metadata. Defaults can be provided in the base skill config.
+ *
+ * This means that other items can have armor values too. Like a shield (iron-door, etc.).
+ *
  * @author Silthus
  */
 @SkillInformation(
@@ -50,16 +57,19 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
 
         allowedArmor = new EnumMap<>(Material.class);
         playerArmor = new EnumMap<>(ItemUtil.ArmorSlot.class);
-        ConfigurationSection section;
-        for (String key : data.getConfigurationSection("armor").getKeys(false)) {
+        // lets load all items that the class can wear
+        for (String key : data.getConfigurationSection("items").getKeys(false)) {
             Material item = ItemUtils.getItem(key);
             if (item != null) {
-                section = data.getConfigurationSection("armor." + key);
                 ItemUtil.ArmorSlot slot = ItemUtil.ArmorSlot.fromMaterial(item);
                 if (slot != null) {
                     if (!allowedArmor.containsKey(item)) {
-                        allowedArmor.put(item, new ArmorPiece(item, slot, section));
+                        int defArmorValue = data.getInt("defaults." + item.getId(), 0);
+                        if (defArmorValue == 0) defArmorValue = data.getInt("defaults." + item.name(), 0);
+                        allowedArmor.put(item, new ArmorPiece(item, slot, defArmorValue, data.getInt("items." + key, 1)));
                     }
+                } else {
+                    RaidCraft.LOGGER.warning("Item " + key + " in der Skill Config: " + getName() + " ist kein Rüstungs Item.");
                 }
             } else {
                 RaidCraft.LOGGER.warning("Item " + key + " in der Skill Config: " + getName() + " wurde nicht erkannt.");
@@ -89,13 +99,17 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
         if (trigger.getAttack().hasSource(AttackSource.ENVIRONMENT)) {
             return;
         }
-        int damageReduction = 0;
+        int totalArmor = 0;
         for (ArmorPiece armor : playerArmor.values()) {
             // lets add up the defence points and reduce the damage
-            damageReduction += armor.getTotalDamageReduction(this);
+            totalArmor += armor.getArmorValue();
         }
-        if (damageReduction > 0) {
-            trigger.getAttack().setDamage(trigger.getAttack().getDamage() - damageReduction);
+        // in percent
+        double damageReduction = getDamageReduction(trigger.getAttack(), totalArmor);
+        int newDamage = (int) (trigger.getAttack().getDamage() - (trigger.getAttack().getDamage() * damageReduction));
+
+        if (newDamage > 0) {
+            trigger.getAttack().setDamage(newDamage);
             getHero().debug("damage reduced by " + damageReduction);
             getHero().combatLog("Schaden durch Rüstung um " + damageReduction + " reduziert.");
         }
@@ -107,14 +121,46 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
         checkArmor();
     }
 
-    public void onInventoryClickTrigger(InventoryClickTrigger trigger) {
+    @TriggerHandler
+    public void onInventoryClick(InventoryClickTrigger trigger) {
 
         checkArmor();
     }
 
+    @TriggerHandler
+    public void onItemHeldChange(ItemHeldTrigger trigger) {
+
+        checkArmor();
+    }
+
+    /**
+     * This reduction formula is based on the WoW Armor Reduction formula for characters up to level 59.
+     * %Reduction = (Armor / ([85 * Enemy_Level] + Armor + 400))
+     * The reduction is always capped at 75% so nobdy can receive 0 damage from armor reduction.
+     *
+     * To make things easier we calculate with a enemy level of 60 at all times.
+     * BUT you can change this when spawning your creature (e.g. boss).
+     *
+     * Since we have about half the armor items (4 opposed to 8) the formula is halfed.
+     *
+     * @param armor that reduces the damage
+     * @return damage reduction in percent
+     */
+    private double getDamageReduction(Attack attack, int armor) {
+
+        int level = 1;
+        if (attack.getSource() instanceof Levelable) {
+            level = ((Levelable) attack.getSource()).getLevel().getLevel();
+        }
+        double reduction = armor / ((45.0 * level) + armor + 200.0);
+        // cap reduction at 75%
+        if (reduction > 0.75) reduction = 0.75;
+        return reduction;
+    }
+
     private boolean isAllowedItem(Material material) {
 
-        return allowedArmor.containsKey(material);
+        return allowedArmor.containsKey(material) && allowedArmor.get(material).getLevel() < getLevel().getLevel();
     }
 
     private boolean isAllowedItem(ItemStack itemStack) {
@@ -143,6 +189,18 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
             inventory.setBoots(null);
             movedItem = true;
         }
+        // lets check if we need to add the held item as a shield
+        ItemUtil.ArmorSlot shield = ItemUtil.ArmorSlot.fromMaterial(inventory.getItemInHand().getType());
+        if (shield != null && allowedArmor.containsKey(inventory.getItemInHand().getType())) {
+            ArmorPiece armor = allowedArmor.get(inventory.getItemInHand().getType());
+            if (armor.getLevel() < getLevel().getLevel()) {
+                playerArmor.put(armor.getSlot(), armor);
+            } else {
+                playerArmor.remove(ItemUtil.ArmorSlot.SHIELD);
+            }
+        } else {
+            playerArmor.remove(ItemUtil.ArmorSlot.SHIELD);
+        }
         if (movedItem) {
             // inform the player
             hero.sendMessage(ChatColor.RED + "Du kannst diese Rüstung nicht tragen. Sie wurde in dein Inventar gelegt.");
@@ -165,17 +223,23 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
         return false;
     }
 
+    /**
+     * The armor piece just holds the information about what slot the item is in
+     * and what armor value the item provides.
+     */
     public static class ArmorPiece {
 
         private final Material type;
         private final ItemUtil.ArmorSlot slot;
-        private final ConfigurationSection section;
+        private final int armorValue;
+        private final int level;
 
-        public ArmorPiece(Material type, ItemUtil.ArmorSlot slot, ConfigurationSection section) {
+        public ArmorPiece(Material type, ItemUtil.ArmorSlot slot, int armorValue, int level) {
 
             this.type = type;
             this.slot = slot;
-            this.section = section;
+            this.armorValue = armorValue;
+            this.level = level;
         }
 
         public Material getType() {
@@ -188,32 +252,14 @@ public class Armor extends AbstractLevelableSkill implements Triggered {
             return slot;
         }
 
-        public double getTotalDamageReduction(Skill skill) {
+        public int getArmorValue() {
 
-            return getDamageReduction()
-                    + getReductionLevelModifier() * skill.getHero().getLevel().getLevel()
-                    + getReductionProfLevelModifier() * skill.getProfession().getLevel().getLevel()
-                    + (skill instanceof Levelable ? getReductionSkillLevelModifier() * ((Levelable) skill).getLevel().getLevel() : 0);
+            return armorValue;
         }
 
-        public double getDamageReduction() {
+        public int getLevel() {
 
-            return section.getDouble("reduction.base", 0.0);
-        }
-
-        public double getReductionLevelModifier() {
-
-            return section.getDouble("reduction.level-modifier", 0.0);
-        }
-
-        public double getReductionProfLevelModifier() {
-
-            return section.getDouble("reduction.prof-level-modifier", 0.0);
-        }
-
-        public double getReductionSkillLevelModifier() {
-
-            return section.getDouble("reduction.skill-level-modifier", 0.0);
+            return level;
         }
 
         @Override
