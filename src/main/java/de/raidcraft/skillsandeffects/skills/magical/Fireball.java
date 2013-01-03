91660e4e3d2798ac60cda3a59df8558f087840da
Silthus <silthus@raid-craft.de>
2013-01-03 21:23:05 +0100

    forgot fireball information

--- src/main/java/de/raidcraft/skillsandeffects/skills/magical/Fireball.java
+++ src/main/java/de/raidcraft/skillsandeffects/skills/magical/Fireball.java
@@ -2,6 +2,8 @@ package de.raidcraft.skillsandeffects.skills.magical;
 
 import com.sk89q.minecraft.util.commands.CommandContext;
 import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
 import de.raidcraft.skills.api.combat.ProjectileType;
 import de.raidcraft.skills.api.combat.action.RangedAttack;
 import de.raidcraft.skills.api.combat.callback.RangedCallback;
@@ -10,6 +12,7 @@ import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.persistance.SkillProperties;
 import de.raidcraft.skills.api.profession.Profession;
 import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.CommandTriggered;
 import de.raidcraft.skills.effects.damaging.Burn;
 import de.raidcraft.skills.tables.THeroSkill;
@@ -17,6 +20,13 @@ import de.raidcraft.skills.tables.THeroSkill;
 /**
  * @author Silthus
  */
+@SkillInformation(
+        name = "Fireball",
+        desc = "Schießt einen Feuerball auf den Gegener",
+        types = {EffectType.MAGICAL, EffectType.DAMAGING, EffectType.SILENCABLE, EffectType.HARMFUL},
+        elements = {EffectElement.FIRE},
+        triggerCombat = true
+)
 public class Fireball extends AbstractLevelableSkill implements CommandTriggered {
 
     public Fireball(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
