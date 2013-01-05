273d4b128909f193ee21a4f11b37d54238534cbf
Silthus <silthus@raid-craft.de>
2013-01-05 09:39:39 +0100

    made strike a multi effect skill with optional damage

--- src/main/java/de/raidcraft/skillsandeffects/skills/physical/Strike.java
+++ src/main/java/de/raidcraft/skillsandeffects/skills/physical/Strike.java
@@ -8,14 +8,16 @@ import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.persistance.SkillProperties;
 import de.raidcraft.skills.api.profession.Profession;
-import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
+import de.raidcraft.skills.api.skill.AbstractSkill;
 import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.CommandTriggered;
 import de.raidcraft.skills.api.trigger.Triggered;
 import de.raidcraft.skills.effects.damaging.Bleed;
 import de.raidcraft.skills.effects.disabling.KnockBack;
+import de.raidcraft.skills.effects.disabling.Stun;
 import de.raidcraft.skills.tables.THeroSkill;
 import de.raidcraft.skills.trigger.AttackTrigger;
+import de.raidcraft.skillsandeffects.effects.armor.SunderingArmor;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
@@ -27,10 +29,12 @@ import org.bukkit.configuration.ConfigurationSection;
         types = {EffectType.PHYSICAL, EffectType.DAMAGING, EffectType.HARMFUL, EffectType.INTERRUPT},
         triggerCombat = true
 )
-public class Strike extends AbstractLevelableSkill implements CommandTriggered, Triggered {
+public class Strike extends AbstractSkill implements CommandTriggered, Triggered {
 
-    private boolean knockBack = true;
+    private boolean knockBack = false;
     private boolean bleed = false;
+    private boolean stun = false;
+    private boolean sunderArmor = false;
 
     public Strike(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
@@ -40,8 +44,10 @@ public class Strike extends AbstractLevelableSkill implements CommandTriggered, 
     @Override
     public void load(ConfigurationSection data) {
 
-        knockBack = data.getBoolean("knockback", true);
+        knockBack = data.getBoolean("knockback", false);
         bleed = data.getBoolean("bleed", false);
+        stun = data.getBoolean("stun", false);
+        sunderArmor = data.getBoolean("sunder-armor", false);
     }
 
     @Override
@@ -53,6 +59,8 @@ public class Strike extends AbstractLevelableSkill implements CommandTriggered, 
 
                 if (knockBack) Strike.this.addEffect(getHero().getEntity().getLocation(), trigger.getAttack().getTarget(), KnockBack.class);
                 if (bleed) Strike.this.addEffect(trigger.getAttack().getTarget(), Bleed.class);
+                if (stun) Strike.this.addEffect(trigger.getAttack().getTarget(), Stun.class);
+                if (sunderArmor) Strike.this.addEffect(trigger.getAttack().getTarget(), SunderingArmor.class);
             }
         });
     }
