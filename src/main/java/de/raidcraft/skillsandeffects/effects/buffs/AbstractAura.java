fc135b47dbee6d3952bed7d447912a5892e3bed8
Silthus <silthus@raid-craft.de>
2013-01-07 18:05:43 +0100

    added annotation to surpress the missing info warning on abstract or unwanted effect/skill classes

--- src/main/java/de/raidcraft/skillsandeffects/effects/buffs/AbstractAura.java
+++ src/main/java/de/raidcraft/skillsandeffects/effects/buffs/AbstractAura.java
@@ -2,6 +2,7 @@ package de.raidcraft.skillsandeffects.effects.buffs;
 
 import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.effect.ExpirableEffect;
+import de.raidcraft.skills.api.effect.IgnoredEffect;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.EffectData;
 import de.raidcraft.skillsandeffects.skills.buffs.Aura;
@@ -9,6 +10,7 @@ import de.raidcraft.skillsandeffects.skills.buffs.Aura;
 /**
  * @author Silthus
  */
+@IgnoredEffect
 public abstract class AbstractAura extends ExpirableEffect<Aura> {
 
     public AbstractAura(Aura source, CharacterTemplate target, EffectData data) {
