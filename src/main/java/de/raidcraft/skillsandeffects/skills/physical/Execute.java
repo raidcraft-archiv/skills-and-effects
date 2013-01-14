5e51a764206489d6baddada7b11e10cb1ab0fa45
Silthus <silthus@raid-craft.de>
2013-01-14 19:27:38 +0100

    added execute skill and fixed bloodstrike

--- src/main/java/de/raidcraft/skillsandeffects/skills/physical/Execute.java
+++ src/main/java/de/raidcraft/skillsandeffects/skills/physical/Execute.java
@@ -1,7 +1,7 @@
 package de.raidcraft.skillsandeffects.skills.physical;
 
 import com.sk89q.minecraft.util.commands.CommandContext;
-import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.callback.Callback;
 import de.raidcraft.skills.api.effect.common.QueuedAttack;
 import de.raidcraft.skills.api.exceptions.CombatException;
@@ -9,40 +9,43 @@ import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.persistance.SkillProperties;
 import de.raidcraft.skills.api.profession.Profession;
 import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
-import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.CommandTriggered;
 import de.raidcraft.skills.tables.THeroSkill;
 import de.raidcraft.skills.trigger.AttackTrigger;
-import de.raidcraft.skillsandeffects.effects.buffs.damage.BloodlustEffect;
+import org.bukkit.configuration.ConfigurationSection;
 
 /**
  * @author Silthus
  */
-@SkillInformation(
-        name = "Blood Strike",
-        desc = "Zerfetzt deinen Gegner mit all deiner Wut.",
-        types = {EffectType.PHYSICAL, EffectType.HARMFUL, EffectType.DAMAGING}
-)
-public class BloodStrike extends AbstractLevelableSkill implements CommandTriggered {
+public class Execute extends AbstractLevelableSkill implements CommandTriggered {
 
-    public BloodStrike(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+    private double healthTreshold = 0.20;
+    private double damagePerResource = 0.5;
+
+    public Execute(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
         super(hero, data, profession, database);
     }
 
     @Override
+    public void load(ConfigurationSection data) {
+
+        healthTreshold = data.getDouble("health-threshhold", 0.20);
+        damagePerResource = data.getDouble("damage-per-resource", 0.5);
+    }
+
+    @Override
     public void runCommand(CommandContext args) throws CombatException {
 
-        final BloodlustEffect effect = getHero().getEffect(BloodlustEffect.class);
-        if (!getHero().hasEffect(BloodlustEffect.class)) {
-            throw new CombatException("Du benötigst mindestens einen Stack " + effect.getFriendlyName());
-        }
         addEffect(getHero(), QueuedAttack.class).addCallback(new Callback<AttackTrigger>() {
             @Override
             public void run(AttackTrigger trigger) throws CombatException {
 
-                trigger.getAttack().setDamage(getTotalDamage() * effect.getStacks());
-                getHero().removeEffect(BloodlustEffect.class);
+                CharacterTemplate target = trigger.getAttack().getTarget();
+                if (target.getHealth() / target.getMaxHealth() > healthTreshold) {
+                    throw new CombatException("Dein Ziel muss unter " + healthTreshold * 100 + "% Leben haben.");
+                }
+                trigger.getAttack().setDamage((int) (getTotalDamage() + damagePerResource * getHero().getResource()));
             }
         });
     }
