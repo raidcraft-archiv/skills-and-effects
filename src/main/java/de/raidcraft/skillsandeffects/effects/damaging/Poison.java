e3dc248a76012977d53e882c03e4c7292c3a0b36
Silthus <silthus@raid-craft.de>
2013-03-20 13:58:41 +0100

    added generic area and cone skills

---
+++ src/main/java/de/raidcraft/skillsandeffects/effects/damaging/Poison.java
@@ -0,0 +1,57 @@
+package de.raidcraft.skillsandeffects.effects.damaging;
+
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.action.EffectDamage;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.PeriodicExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.skill.Skill;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+/**
+ * @author Silthus
+ */
+@EffectInformation(
+        name = "Poison",
+        description = "Das Ziel ist vergiftet.",
+        types = {EffectType.HARMFUL, EffectType.MAGICAL, EffectType.DAMAGING, EffectType.PURGEABLE, EffectType.DEBUFF},
+        elements = {EffectElement.EARTH}
+)
+public class Poison extends PeriodicExpirableEffect<Skill> {
+
+    private PotionEffect potionEffect;
+
+    public Poison(Skill source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+        potionEffect = new PotionEffect(PotionEffectType.POISON, (int) getDuration(), 0, true);
+    }
+
+    @Override
+    protected void tick(CharacterTemplate target) throws CombatException {
+
+        new EffectDamage(this, getDamage()).run();
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+        renew(target);
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+        target.getEntity().removePotionEffect(PotionEffectType.POISON);
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+        target.getEntity().addPotionEffect(potionEffect, true);
+    }
+}
--- src/main/java/de/raidcraft/skillsandeffects/skills/magical/ConeCast.java
+++ src/main/java/de/raidcraft/skillsandeffects/skills/magical/ConeCast.java
@@ -1,9 +1,9 @@
 package de.raidcraft.skillsandeffects.skills.magical;
 
 import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.EffectElement;
 import de.raidcraft.skills.api.combat.EffectType;
-import de.raidcraft.skills.api.combat.MagicalAttackType;
 import de.raidcraft.skills.api.combat.action.EntityAttack;
 import de.raidcraft.skills.api.combat.callback.EntityAttackCallback;
 import de.raidcraft.skills.api.exceptions.CombatException;
@@ -13,7 +13,6 @@ import de.raidcraft.skills.api.profession.Profession;
 import de.raidcraft.skills.api.skill.AbstractSkill;
 import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.CommandTriggered;
-import de.raidcraft.skills.api.trigger.Triggered;
 import de.raidcraft.skills.effects.disabling.Disarm;
 import de.raidcraft.skills.effects.disabling.Interrupt;
 import de.raidcraft.skills.effects.disabling.KnockBack;
@@ -32,12 +31,13 @@ import org.bukkit.configuration.ConfigurationSection;
  * @author Silthus
  */
 @SkillInformation(
-        name = "Magic Bolt",
-        desc = "Greift ein Ziel mit Magie an.",
-        types = {EffectType.MAGICAL, EffectType.SILENCABLE, EffectType.HARMFUL, EffectType.DAMAGING}
+        name = "Cone Cast",
+        desc = "Kegelförmiger Zauber mit einigen Effekten.",
+        types = {EffectType.AREA, EffectType.SILENCABLE, EffectType.MAGICAL, EffectType.HARMFUL}
 )
-public class MagicBolt extends AbstractSkill implements CommandTriggered, Triggered {
+public class ConeCast extends AbstractSkill implements CommandTriggered {
 
+    private float degrees = 45.0F;
     private boolean knockBack = false;
     private boolean bleed = false;
     private boolean stun = false;
@@ -50,7 +50,7 @@ public class MagicBolt extends AbstractSkill implements CommandTriggered, Trigge
     private boolean disable = false;
     private boolean poison = false;
 
-    public MagicBolt(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+    public ConeCast(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
         super(hero, data, profession, database);
     }
@@ -59,6 +59,7 @@ public class MagicBolt extends AbstractSkill implements CommandTriggered, Trigge
     public void load(ConfigurationSection data) {
 
         addElements(EffectElement.fromString(data.getString("element")));
+        degrees = (float) data.getDouble("angle", 45.0);
         knockBack = data.getBoolean("knockback", false);
         bleed = data.getBoolean("bleed", false);
         stun = data.getBoolean("stun", false);
@@ -75,22 +76,25 @@ public class MagicBolt extends AbstractSkill implements CommandTriggered, Trigge
     @Override
     public void runCommand(CommandContext args) throws CombatException {
 
-        magicalAttack(MagicalAttackType.SMOKE, new EntityAttackCallback() {
-            @Override
-            public void run(EntityAttack attack) throws CombatException {
+        for (CharacterTemplate target : getTargetsInFront(degrees)) {
 
-                if (knockBack) MagicBolt.this.addEffect(getHero().getEntity().getLocation(), attack.getTarget(), KnockBack.class);
-                if (bleed) MagicBolt.this.addEffect(attack.getTarget(), Bleed.class);
-                if (stun) MagicBolt.this.addEffect(attack.getTarget(), Stun.class);
-                if (sunderArmor) MagicBolt.this.addEffect(attack.getTarget(), SunderingArmor.class);
-                if (disarm) MagicBolt.this.addEffect(attack.getTarget(), Disarm.class);
-                if (slow) MagicBolt.this.addEffect(attack.getTarget(), Slow.class);
-                if (weaken) MagicBolt.this.addEffect(attack.getTarget(), Weakness.class);
-                if (burn) MagicBolt.this.addEffect(attack.getTarget(), Burn.class);
-                if (interrupt) MagicBolt.this.addEffect(attack.getTarget(), Interrupt.class);
-                if (disable) MagicBolt.this.addEffect(attack.getTarget(), Pigify.class);
-                if (poison) addEffect(attack.getTarget(), Poison.class);
-            }
-        });
+            magicalAttack(target, new EntityAttackCallback() {
+                @Override
+                public void run(EntityAttack attack) throws CombatException {
+
+                    if (knockBack) addEffect(getHero().getEntity().getLocation(), attack.getTarget(), KnockBack.class);
+                    if (bleed) addEffect(attack.getTarget(), Bleed.class);
+                    if (stun) addEffect(attack.getTarget(), Stun.class);
+                    if (sunderArmor) addEffect(attack.getTarget(), SunderingArmor.class);
+                    if (disarm) addEffect(attack.getTarget(), Disarm.class);
+                    if (slow) addEffect(attack.getTarget(), Slow.class);
+                    if (weaken) addEffect(attack.getTarget(), Weakness.class);
+                    if (burn) addEffect(attack.getTarget(), Burn.class);
+                    if (interrupt) addEffect(attack.getTarget(), Interrupt.class);
+                    if (disable) addEffect(attack.getTarget(), Pigify.class);
+                    if (poison) addEffect(attack.getTarget(), Poison.class);
+                }
+            }).run();
+        }
     }
 }
