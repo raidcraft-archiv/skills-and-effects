829a9e5f151e064f70e7b90da29008f48597a645
Silthus <silthus@raid-craft.de>
2013-03-23 10:32:25 +0100

    added silktouch skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pve/effects/SilktouchEffect.java
@@ -0,0 +1,80 @@
+package de.raidcraft.skillsandeffects.pve.effects;
+
+import com.sk89q.worldedit.blocks.ItemID;
+import de.raidcraft.RaidCraft;
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.effect.ExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.BlockBreakTrigger;
+import de.raidcraft.skillsandeffects.pve.skills.Silktouch;
+import de.raidcraft.util.ItemUtils;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Silktouch",
+        desc = "Ermöglicht es Blöcke in ihrer Ursprungsform abzubauen."
+)
+public class SilktouchEffect extends ExpirableEffect<Silktouch> implements Triggered {
+
+    private final List<Integer> blockIds = new ArrayList<>();
+    private final ItemStack silkTouch = new ItemStack(ItemID.DIAMOND_PICKAXE);
+
+    public SilktouchEffect(Silktouch source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+        this.silkTouch.addEnchantment(Enchantment.SILK_TOUCH, 1);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        for (String key : data.getStringList("blocks")) {
+            Material item = ItemUtils.getItem(key);
+            if (item == null) {
+                RaidCraft.LOGGER.warning("Uknown item " + key + " in effect config of " + getName() + " for skill " + getSource().getName());
+                continue;
+            }
+            blockIds.add(item.getId());
+        }
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakTrigger trigger) {
+
+        Block block = trigger.getEvent().getBlock();
+        if (blockIds.contains(block.getTypeId())) {
+            // simply break the block with a different tool and cancel the event
+            block.breakNaturally(silkTouch);
+            trigger.getEvent().setCancelled(true);
+        }
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pve/skills/Silktouch.java
@@ -0,0 +1,33 @@
+package de.raidcraft.skillsandeffects.pve.skills;
+
+import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.CommandTriggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skillsandeffects.pve.effects.SilktouchEffect;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Silktouch",
+        desc = "Ermöglicht es Blöcke ohne die Verzauberung Silktouch genauso abzubauen."
+)
+public class Silktouch extends AbstractSkill implements CommandTriggered {
+
+    public Silktouch(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void runCommand(CommandContext args) throws CombatException {
+
+        addEffect(getHero(), SilktouchEffect.class);
+    }
+}
