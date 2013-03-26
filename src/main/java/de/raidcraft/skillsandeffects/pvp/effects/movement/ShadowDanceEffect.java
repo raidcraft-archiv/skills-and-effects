e7f02a0e34fd7eb202f631b352652c3caaf4ed5f
Silthus <silthus@raid-craft.de>
2013-03-26 18:32:14 +0100

    added shadow dance skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/movement/ShadowDanceEffect.java
@@ -0,0 +1,94 @@
+package de.raidcraft.skillsandeffects.pvp.effects.movement;
+
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.PeriodicExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.skill.Skill;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.PlayerCastSkillTrigger;
+import de.raidcraft.skillsandeffects.pvp.effects.buffs.generic.DamageBuff;
+import de.raidcraft.skillsandeffects.pvp.skills.movement.ShadowDance;
+import de.raidcraft.skillsandeffects.pvp.skills.movement.ShadowStep;
+import de.raidcraft.util.MathUtil;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.List;
+
+/**
+ * @author Silthus
+ */
+@EffectInformation(
+        name = "Shadow Dance",
+        description = "Springt von Ziel zu ziel und fügt Schaden zu.",
+        types = {EffectType.HELPFUL, EffectType.BUFF, EffectType.MOVEMENT}
+)
+public class ShadowDanceEffect extends PeriodicExpirableEffect<ShadowDance> implements Triggered {
+
+    private String resource;
+    private boolean bonusDamage = false;
+
+    public ShadowDanceEffect(ShadowDance source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        resource = data.getString("resource");
+        bonusDamage = data.getBoolean("bonus-damage", false);
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.LOWEST)
+    public void onSkillCast(PlayerCastSkillTrigger trigger) {
+
+        if (resource == null) {
+            return;
+        }
+        trigger.getAction().setResourceCost(resource, 0);
+    }
+
+    @Override
+    protected void tick(CharacterTemplate target) throws CombatException {
+
+        apply(target);
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+        List<CharacterTemplate> targets = target.getNearbyTargets(getSource().getTotalRange(), false);
+        if (targets.size() < 1) {
+            return;
+        }
+        shadowStep(targets);
+
+        if (bonusDamage) {
+            target.addEffect((Skill) getSource(), DamageBuff.class);
+        }
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+        if (bonusDamage) {
+            target.removeEffect(DamageBuff.class);
+        }
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+        apply(target);
+    }
+
+    private void shadowStep(List<CharacterTemplate> targets) throws CombatException {
+
+        ShadowStep.shadowStep(getTarget(), targets.get(MathUtil.RANDOM.nextInt(targets.size())));
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/movement/ShadowDance.java
@@ -0,0 +1,35 @@
+package de.raidcraft.skillsandeffects.pvp.skills.movement;
+
+import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.CommandTriggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skillsandeffects.pvp.effects.movement.ShadowDanceEffect;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Shadow Dance",
+        description = "Springt mit Shadow Step von Ziel zu Ziel.",
+        types = {EffectType.HELPFUL, EffectType.BUFF, EffectType.DAMAGING, EffectType.MOVEMENT}
+)
+public class ShadowDance extends AbstractSkill implements CommandTriggered {
+
+    public ShadowDance(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void runCommand(CommandContext args) throws CombatException {
+
+        addEffect(getHero(), ShadowDanceEffect.class);
+    }
+}
