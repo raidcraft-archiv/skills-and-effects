25d5edfb623649ff8ebfcaa71b9d78b5ca65dfdd
Silthus <silthus@raid-craft.de>
2013-03-27 18:39:45 +0100

    added weapon bonus damage skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/weapons/WeaponBonusDamage.java
@@ -0,0 +1,82 @@
+package de.raidcraft.skillsandeffects.pvp.skills.weapons;
+
+import de.raidcraft.RaidCraft;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.action.WeaponAttack;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.items.WeaponType;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.trigger.AttackTrigger;
+import de.raidcraft.skills.util.ConfigUtil;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Weapon Bonus",
+        description = "Ermöglicht es mehr Schaden mit bestimmten Waffen zu verursachen.",
+        types = {EffectType.PHYSICAL, EffectType.DEFAULT_ATTACK, EffectType.DAMAGING, EffectType.HELPFUL}
+)
+public class WeaponBonusDamage extends AbstractLevelableSkill implements Triggered {
+
+    private final Map<WeaponType, ConfigurationSection> bonusDamage = new EnumMap<>(WeaponType.class);
+    private boolean allAttacks = false;
+
+    public WeaponBonusDamage(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        allAttacks = data.getBoolean("all-attacks", false);
+        for (String key : data.getKeys(false)) {
+            WeaponType type = WeaponType.fromString(key);
+            if (type != null) {
+                bonusDamage.put(type, data.getConfigurationSection(key));
+            } else {
+                RaidCraft.LOGGER.warning("Unknown Weapon Type " + key + " in skill config: " + getName() + ".yml");
+            }
+        }
+    }
+
+    public double getBonusDamage(WeaponType weaponType) {
+
+        if (bonusDamage.containsKey(weaponType)) {
+            return ConfigUtil.getTotalValue(this, bonusDamage.get(weaponType));
+        }
+        return 0;
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.LOWEST)
+    public void onAttack(AttackTrigger trigger) {
+
+        if (!allAttacks && !trigger.getAttack().isOfAttackType(EffectType.DEFAULT_ATTACK)) {
+            return;
+        }
+        if (!(trigger.getAttack() instanceof WeaponAttack)) {
+            return;
+        }
+        WeaponAttack attack = (WeaponAttack) trigger.getAttack();
+        if (!bonusDamage.containsKey(attack.getWeapon().getWeaponType())) {
+            return;
+        }
+        int oldDamage = attack.getDamage();
+        double bonusDamage = getBonusDamage(attack.getWeapon().getWeaponType());
+        int newDamage = (int) (oldDamage + oldDamage * bonusDamage);
+        attack.combatLog(this, "Waffenschaden um " + (newDamage - oldDamage) + "(" + ((int) (bonusDamage * 100)) + "%) erhöht.");
+        attack.setDamage(newDamage);
+    }
+}
