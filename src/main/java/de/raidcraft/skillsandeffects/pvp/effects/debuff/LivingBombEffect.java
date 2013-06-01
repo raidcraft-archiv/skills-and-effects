af37f58b54f1d7c879faa0ac791561a867a1ec84
Silthus <silthus@raid-craft.de>
2013-06-01 23:19:44 +0200

    fixed living bomb

--- src/main/java/de/raidcraft/skillsandeffects/pvp/effects/debuff/LivingBombEffect.java
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/debuff/LivingBombEffect.java
@@ -3,15 +3,12 @@ package de.raidcraft.skillsandeffects.pvp.effects.debuff;
 import de.raidcraft.skills.api.character.CharacterTemplate;
 import de.raidcraft.skills.api.combat.EffectElement;
 import de.raidcraft.skills.api.combat.EffectType;
-import de.raidcraft.skills.api.combat.action.EntityAttack;
-import de.raidcraft.skills.api.combat.callback.EntityAttackCallback;
 import de.raidcraft.skills.api.effect.EffectInformation;
 import de.raidcraft.skills.api.effect.types.ExpirableEffect;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.EffectData;
 import de.raidcraft.skillsandeffects.pvp.skills.magical.LivingBomb;
 import de.raidcraft.util.EffectUtil;
-import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.util.Vector;
 
@@ -51,24 +48,15 @@ public class LivingBombEffect extends ExpirableEffect<LivingBomb> {
 
         for (final CharacterTemplate victim : target.getNearbyTargets(blastRadius)) {
 
-            if (!victim.isFriendly(getSource().getHolder())) {
-                getSource().magicalAttack(target, getDamage(), new EntityAttackCallback() {
-                    @Override
-                    public void run(EntityAttack attack) throws CombatException {
-
-                        // create an explosion that does not damage
-                        Location location = attack.getTarget().getEntity().getLocation();
-                        EffectUtil.fakeExplosion(location);
-                        // and launch the target into the air
-                        attack.getTarget().getEntity().setVelocity(new Vector(0, 1, 0));
-                        hit++;
-                    }
-                });
+            if (!victim.equals(target)) {
+                getSource().magicalAttack(victim, getDamage());
+                hit++;
             }
         }
         if (hit > 0) {
             // launch the target high into the air
-            target.getEntity().setVelocity(new Vector(0, 3, 0));
+            target.getEntity().setVelocity(new Vector(0, 3 + hit, 0));
+            EffectUtil.fakeExplosion(target.getEntity().getLocation());
         }
     }
 
