b66991158c69c9f0429371d8d1ad5352013cadee
Silthus <silthus@raid-craft.de>
2013-06-01 21:48:27 +0200

    meteor can specify amount

--- src/main/java/de/raidcraft/skillsandeffects/pvp/skills/magical/Meteor.java
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/magical/Meteor.java
@@ -14,8 +14,10 @@ import de.raidcraft.skills.api.skill.AbstractSkill;
 import de.raidcraft.skills.api.skill.SkillInformation;
 import de.raidcraft.skills.api.trigger.CommandTriggered;
 import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.util.ConfigUtil;
 import de.raidcraft.util.MathUtil;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.util.Vector;
 
 /**
@@ -29,9 +31,23 @@ import org.bukkit.util.Vector;
 )
 public class Meteor extends AbstractSkill implements CommandTriggered {
 
+    private ConfigurationSection amount;
+
     public Meteor(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
         super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        amount = data.getConfigurationSection("amount");
+    }
+
+    public int getAmount() {
+
+        int value = (int) ConfigUtil.getTotalValue(this, amount);
+        return value > 0 ? value : 1;
     }
 
     @Override
@@ -44,17 +60,20 @@ public class Meteor extends AbstractSkill implements CommandTriggered {
 
         // lets get a position above the location with a random offset
         Location origin = location.clone();
-        origin = origin.add(MathUtil.RANDOM.nextInt(3), MathUtil.RANDOM.nextInt(5) + 3, MathUtil.RANDOM.nextInt(3));
-        RangedAttack<ProjectileCallback> attack = new RangedAttack<>(getHolder(), ProjectileType.LARGE_FIREBALL, getTotalDamage());
-        org.bukkit.entity.Fireball projectile = location.getWorld().spawn(origin, org.bukkit.entity.Fireball.class);
-        projectile.setShooter(getHolder().getPlayer());
-        projectile.setIsIncendiary(true);
-        projectile.setFireTicks(100);
-        attack.setProjectile(projectile);
 
-        Vector direction = new Vector(location.getX() - origin.getX(), location.getY() - origin.getY(), location.getZ() - origin.getZ());
-        direction = direction.normalize();
-        attack.setVelocity(direction);
-        attack.run();
+        for (int i = 0; i < getAmount(); i++) {
+            origin = origin.add(MathUtil.RANDOM.nextInt(3), MathUtil.RANDOM.nextInt(5) + 3, MathUtil.RANDOM.nextInt(3));
+            RangedAttack<ProjectileCallback> attack = new RangedAttack<>(getHolder(), ProjectileType.LARGE_FIREBALL, getTotalDamage());
+            org.bukkit.entity.Fireball projectile = location.getWorld().spawn(origin, org.bukkit.entity.Fireball.class);
+            projectile.setShooter(getHolder().getPlayer());
+            projectile.setIsIncendiary(true);
+            projectile.setFireTicks(100);
+            attack.setProjectile(projectile);
+
+            Vector direction = new Vector(location.getX() - origin.getX(), location.getY() - origin.getY(), location.getZ() - origin.getZ());
+            direction = direction.normalize().multiply(i);
+            attack.setVelocity(direction);
+            attack.run();
+        }
     }
 }
