248939d968b41206cf20a2e20b6a30280b8b83c5
Silthus <silthus@raid-craft.de>
2013-06-02 15:31:55 +0200

    added boneshield effect & skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/buffs/protection/BoneShieldEffect.java
@@ -0,0 +1,65 @@
+package de.raidcraft.skillsandeffects.pvp.effects.buffs.protection;
+
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.types.ExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.DamageTrigger;
+import de.raidcraft.skillsandeffects.pvp.skills.buffs.BoneShield;
+
+/**
+ * @author Silthus
+ */
+@EffectInformation(
+        name = "Bone Shield",
+        description = "Schütz das Ziel mit Knochen und absorbiert eingehenden Schaden.",
+        types = {EffectType.MAGICAL, EffectType.BUFF, EffectType.PURGEABLE, EffectType.ABSORBING}
+)
+public class BoneShieldEffect extends ExpirableEffect<BoneShield> implements Triggered {
+
+    private int damageToAbsorb;
+
+    public BoneShieldEffect(BoneShield source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onDamage(DamageTrigger trigger) throws CombatException {
+
+        if (damageToAbsorb <= 0) {
+            remove();
+            return;
+        }
+        int damage = trigger.getAttack().getDamage();
+        int newDamage = damage - damageToAbsorb;
+        if (newDamage < 0) {
+            newDamage = 0;
+        }
+        damageToAbsorb -= damage;
+        combatLog(damage - newDamage + " von " + damage + " Schaden absorbiert.");
+        trigger.getAttack().setDamage(newDamage);
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+        renew(target);
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+        damageToAbsorb = (int) getSource().getAbsorbtion();
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/buffs/BoneShield.java
@@ -0,0 +1,86 @@
+package de.raidcraft.skillsandeffects.pvp.skills.buffs;
+
+import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.resource.Resource;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.CommandTriggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.util.ConfigUtil;
+import de.raidcraft.skillsandeffects.pvp.effects.buffs.protection.BoneShieldEffect;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Bone Shield",
+        description = "Schütz das Ziel mit Knochen",
+        types = {EffectType.MAGICAL, EffectType.BUFF, EffectType.HELPFUL, EffectType.ABSORBING}
+)
+public class BoneShield extends AbstractSkill implements CommandTriggered {
+
+    private boolean current;
+    private Resource resource;
+    private ConfigurationSection absorbtion;
+    private ConfigurationSection maxTargets;
+
+    private List<CharacterTemplate> affectedTargets = new ArrayList<>();
+
+    public BoneShield(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        current = data.getBoolean("current", true);
+        resource = getHolder().getResource(data.getString("resource"));
+        absorbtion = data.getConfigurationSection("absorbtion");
+        maxTargets = data.getConfigurationSection("max-targets");
+    }
+
+    public int getMaxTargets() {
+
+        double value = ConfigUtil.getTotalValue(this, maxTargets);
+        return value > 0 ? (int) value : 1;
+    }
+
+    public double getAbsorbtion() {
+
+        if (resource == null) {
+            return 0.0;
+        }
+        double value;
+        if (current) {
+            value = resource.getCurrent();
+        } else {
+            value = resource.getMax();
+        }
+        return value * ConfigUtil.getTotalValue(this, absorbtion);
+    }
+
+    @Override
+    public void runCommand(CommandContext args) throws CombatException {
+
+        if (resource == null) {
+            throw new CombatException("Wrong resource defined! Please fix your config...");
+        }
+        if (affectedTargets.size() >= getMaxTargets() && affectedTargets.size() > 0) {
+            affectedTargets.remove(0).removeEffect(BoneShieldEffect.class);
+        }
+        CharacterTemplate target = getTarget(args, true, false);
+        addEffect(target, BoneShieldEffect.class);
+        affectedTargets.add(target);
+    }
+}
