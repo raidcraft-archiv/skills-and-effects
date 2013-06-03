f8f0dd50967996fe3e9cee0c73c54ae826ed054c
Silthus <silthus@raid-craft.de>
2013-06-03 14:06:34 +0200

    added arch angel skill and effect

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/buffs/healing/ArchAngelEffect.java
@@ -0,0 +1,81 @@
+package de.raidcraft.skillsandeffects.pvp.effects.buffs.healing;
+
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.action.HealAction;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.types.ExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.DamageTrigger;
+import de.raidcraft.skills.trigger.HealTrigger;
+import de.raidcraft.skillsandeffects.pvp.skills.protection.ArchAngel;
+import de.raidcraft.util.MathUtil;
+
+/**
+ * @author Silthus
+ */
+@EffectInformation(
+        name = "Arch Angel",
+        description = "Schütz das Ziel vor dem sicheren Tod.",
+        types = {EffectType.BUFF, EffectType.HEALING, EffectType.PROTECTION, EffectType.MAGICAL, EffectType.HELPFUL},
+        elements = {EffectElement.HOLY}
+)
+public class ArchAngelEffect extends ExpirableEffect<ArchAngel> implements Triggered {
+
+    public ArchAngelEffect(ArchAngel source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.HIGHEST)
+    public void onDamage(DamageTrigger trigger) throws CombatException {
+
+        CharacterTemplate target = trigger.getAttack().getTarget();
+        if (target.getHealth() - trigger.getAttack().getDamage() > 0) {
+            return;
+        }
+        // cancel the attack and save the target
+        remove();
+        new HealAction<>(this, target, (int) (getSource().getDeathHealAmount() * target.getMaxHealth())).run();
+        info("Du wurdest von " + getFriendlyName() + " vor dem Tod bewahrt!");
+        throw new CombatException("Der tödliche Schaden wurde von " + getFriendlyName() + " verhindert.");
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onHeal(HealTrigger trigger) {
+
+        int oldAmount = trigger.getAmount();
+        double bonusHealAmount = getSource().getBonusHealAmount();
+        int newAmount = (int) (oldAmount + oldAmount * bonusHealAmount);
+        combatLog("Erhaltene Heilung um " + (newAmount - oldAmount) + "(" + MathUtil.toPercent(bonusHealAmount) + "%) erhöht.");
+        trigger.setAmount(newAmount);
+
+        // also give back some mana
+        if (trigger.getSource().equals(getSource().getHolder())) {
+            getSource().giveResourceBonus(trigger.getAction());
+        }
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/protection/ArchAngel.java
@@ -0,0 +1,76 @@
+package de.raidcraft.skillsandeffects.pvp.skills.protection;
+
+import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.action.HealAction;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.resource.Resource;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.Skill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.CommandTriggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.util.ConfigUtil;
+import org.bukkit.configuration.ConfigurationSection;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Arch Angel",
+        description = "Beschützt das Ziel vor dem sicheren Tod.",
+        types = {EffectType.MAGICAL, EffectType.BUFF, EffectType.HELPFUL, EffectType.HEALING},
+        elements = {EffectElement.HOLY}
+)
+public class ArchAngel extends AbstractSkill implements CommandTriggered {
+
+    private ConfigurationSection deathHealAmount;
+    private ConfigurationSection bonusHealAmount;
+    private ConfigurationSection resourceBonus;
+    private Resource resource;
+
+    public ArchAngel(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        deathHealAmount = data.getConfigurationSection("death-heal-amount");
+        bonusHealAmount = data.getConfigurationSection("bonus-heal-amount");
+        resourceBonus = data.getConfigurationSection("resource-bonus");
+        resource = getHolder().getResource(data.getString("resource"));
+    }
+
+    public void giveResourceBonus(HealAction<?> action) {
+
+        if (resource == null) {
+            return;
+        }
+        if (action.getSource() instanceof Skill) {
+            double resourceCost = ((Skill) action.getSource()).getTotalResourceCost(resource.getName());
+            resource.setCurrent((int) (resource.getCurrent() + resourceCost * ConfigUtil.getTotalValue(this, resourceBonus)));
+        }
+    }
+
+    public double getBonusHealAmount() {
+
+        return ConfigUtil.getTotalValue(this, bonusHealAmount);
+    }
+
+    public double getDeathHealAmount() {
+
+        return ConfigUtil.getTotalValue(this, deathHealAmount);
+    }
+
+    @Override
+    public void runCommand(CommandContext args) throws CombatException {
+
+
+    }
+}
