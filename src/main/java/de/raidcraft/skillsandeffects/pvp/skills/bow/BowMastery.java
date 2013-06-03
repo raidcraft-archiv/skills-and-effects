668d214d2381089991d18662cca4991ada912dc3
Silthus <silthus@raid-craft.de>
2013-06-03 09:27:13 +0200

    added bow mastery skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/bow/BowMastery.java
@@ -0,0 +1,57 @@
+package de.raidcraft.skillsandeffects.pvp.skills.bow;
+
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.trigger.BowFireTrigger;
+import de.raidcraft.skills.util.ConfigUtil;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Bow Mastery",
+        description = "Erhöht die Spannkraft des Bogens",
+        types = {EffectType.PHYSICAL, EffectType.HELPFUL}
+)
+public class BowMastery extends AbstractSkill implements Triggered {
+
+    private ConfigurationSection minimalForce;
+
+    public BowMastery(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        minimalForce = data.getConfigurationSection("minimal-force");
+    }
+
+    public float getMinimalForce() {
+
+        return (float) ConfigUtil.getTotalValue(this, minimalForce);
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onBowFire(BowFireTrigger trigger) {
+
+        float minimalForce = getMinimalForce();
+        if (trigger.getEvent().getForce() < minimalForce) {
+            Entity projectile = trigger.getEvent().getProjectile();
+            Vector velocity = projectile.getVelocity();
+            velocity.multiply(minimalForce + 1);
+            projectile.setVelocity(velocity);
+        }
+    }
+}
