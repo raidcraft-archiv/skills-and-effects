5c57f2be637934cd6491a03aa43bb7b1c758d7de
Silthus <silthus@raid-craft.de>
2013-06-03 08:58:00 +0200

    added pull skill

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/physical/Pull.java
@@ -0,0 +1,61 @@
+package de.raidcraft.skillsandeffects.pvp.skills.physical;
+
+import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.ProjectileType;
+import de.raidcraft.skills.api.combat.callback.RangedCallback;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.CommandTriggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import org.bukkit.Location;
+import org.bukkit.entity.Projectile;
+import org.bukkit.util.Vector;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Pull",
+        description = "Wirft die aktuelle Waffe in Richtung Gegner.",
+        types = {EffectType.PHYSICAL, EffectType.DAMAGING}
+)
+public class Pull extends AbstractSkill implements CommandTriggered {
+
+    public Pull(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+
+    }
+
+    @Override
+    public void runCommand(CommandContext args) throws CombatException {
+
+        Projectile projectile = rangedAttack(ProjectileType.FISH, new RangedCallback() {
+            @Override
+            public void run(CharacterTemplate target) throws CombatException {
+
+                // calculate the velocity and pull the target towards the caster
+                Location sourceLoc = target.getEntity().getLocation();
+                Location targetLoc = getHolder().getEntity().getLocation();
+
+                Vector direction = new Vector(
+                        targetLoc.getX() - sourceLoc.getX(),
+                        targetLoc.getY() - sourceLoc.getY(),
+                        targetLoc.getZ() - sourceLoc.getZ()
+                );
+
+                direction.add(new Vector(0, 1.5, 0));
+                direction.multiply(1.5F);
+
+                target.getEntity().setVelocity(direction);
+            }
+        }).getProjectile();
+        projectile.setVelocity(projectile.getVelocity().multiply(5.0));
+    }
+}
