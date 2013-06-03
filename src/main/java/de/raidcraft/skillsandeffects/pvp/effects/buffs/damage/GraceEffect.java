27be1b4c22f972f21808eae12e2276f5672f33cf
Silthus <silthus@raid-craft.de>
2013-06-03 13:28:48 +0200

    added grace skill and effect

---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/buffs/damage/GraceEffect.java
@@ -0,0 +1,46 @@
+package de.raidcraft.skillsandeffects.pvp.effects.buffs.damage;
+
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.Stackable;
+import de.raidcraft.skills.api.effect.types.ExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skillsandeffects.pvp.skills.passive.FlamingRage;
+
+/**
+ * @author Silthus
+ */
+@EffectInformation(
+        name = "Grace",
+        description = "Verringert die Zauberzeit des nächsten Heilzaubers.",
+        types = {EffectType.PURGEABLE, EffectType.MAGICAL, EffectType.HELPFUL},
+        elements = {EffectElement.HOLY}
+)
+public class GraceEffect extends ExpirableEffect<FlamingRage> implements Stackable {
+
+    public GraceEffect(FlamingRage source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+        setStacks(0);
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/passive/Grace.java
@@ -0,0 +1,57 @@
+package de.raidcraft.skillsandeffects.pvp.skills.passive;
+
+import de.raidcraft.skills.api.combat.EffectElement;
+import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.trigger.PlayerCastSkillTrigger;
+import de.raidcraft.skills.util.ConfigUtil;
+import de.raidcraft.skillsandeffects.pvp.effects.buffs.damage.FlamingRageEffect;
+import de.raidcraft.skillsandeffects.pvp.effects.buffs.damage.GraceEffect;
+import org.bukkit.configuration.ConfigurationSection;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Grace",
+        description = "Verringert die Zauberzeit des nächsten Heilzaubers nach einer heiligen Attacke",
+        types = {EffectType.MAGICAL, EffectType.BUFF, EffectType.HELPFUL},
+        elements = {EffectElement.HOLY}
+)
+public class Grace extends AbstractSkill implements Triggered {
+
+    private double castDecreasePerStack = 0.20;
+
+    public Grace(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        castDecreasePerStack = ConfigUtil.getTotalValue(this, data.getConfigurationSection("casttime-bonus-per-stack"));
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onSkillCast(PlayerCastSkillTrigger trigger) throws CombatException {
+
+        if (trigger.getSkill().isOfElement(EffectElement.HOLY) && trigger.getSkill().isOfType(EffectType.DAMAGING)) {
+            addEffect(getHolder(), GraceEffect.class);
+        } else if (trigger.getSkill().isOfElement(EffectElement.HOLY)
+                && getHolder().hasEffect(FlamingRageEffect.class)
+                && trigger.getSkill().isOfType(EffectType.HEALING)) {
+            int stacks = getHolder().getEffect(GraceEffect.class).getStacks();
+            trigger.getAction().setCastTime((int) (trigger.getAction().getCastTime() - castDecreasePerStack * stacks));
+            getHolder().removeEffect(GraceEffect.class);
+        }
+    }
+}
