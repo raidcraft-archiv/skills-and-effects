8bf47214288f75fe955068fe251bddc7884d2d20
Silthus <silthus@raid-craft.de>
2013-06-13 09:08:15 +0200

    trying to fix the meteor spell

--- src/main/java/de/raidcraft/skillsandeffects/pvp/skills/magical/Meteor.java
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/skills/magical/Meteor.java
@@ -1,6 +1,8 @@
 package de.raidcraft.skillsandeffects.pvp.skills.magical;
 
 import com.sk89q.minecraft.util.commands.CommandContext;
+import de.raidcraft.RaidCraft;
+import de.raidcraft.skills.SkillsPlugin;
 import de.raidcraft.skills.api.combat.EffectElement;
 import de.raidcraft.skills.api.combat.EffectType;
 import de.raidcraft.skills.api.combat.ProjectileType;
@@ -16,8 +18,13 @@ import de.raidcraft.skills.api.trigger.CommandTriggered;
 import de.raidcraft.skills.tables.THeroSkill;
 import de.raidcraft.skills.util.ConfigUtil;
 import de.raidcraft.util.MathUtil;
+import de.raidcraft.util.TimeUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LargeFireball;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 /**
@@ -32,6 +39,12 @@ import org.bukkit.util.Vector;
 public class Meteor extends AbstractSkill implements CommandTriggered {
 
     private ConfigurationSection amount;
+    private BukkitTask meteorTask;
+    private int firedMeteors = 0;
+    private double minSpeed;
+    private double maxSpeed;
+    private long interval;
+    private long delay;
 
     public Meteor(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
@@ -42,6 +55,10 @@ public class Meteor extends AbstractSkill implements CommandTriggered {
     public void load(ConfigurationSection data) {
 
         amount = data.getConfigurationSection("amount");
+        minSpeed = data.getDouble("min-speed", 0.01);
+        maxSpeed = data.getDouble("max-speed", 0.75);
+        interval = TimeUtil.secondsToTicks(data.getDouble("interval", 1.0));
+        delay = TimeUtil.secondsToTicks(data.getDouble("delay", 1.0));
     }
 
     public int getAmount() {
@@ -56,24 +73,45 @@ public class Meteor extends AbstractSkill implements CommandTriggered {
         dropMeteor(getBlockTarget());
     }
 
-    public void dropMeteor(Location location) throws CombatException {
+    public void dropMeteor(final Location location) throws CombatException {
 
         // lets get a position above the location with a random offset
-        Location origin = location.clone();
+        final Location origin = location.clone();
+        final Vector direction = new Vector(location.getX() - origin.getX(),
+                location.getY() - origin.getY(),
+                location.getZ() - origin.getZ()).normalize();
+        // lets start a repeating task to not spawn all meteors at once
+        final int amount = getAmount();
+        firedMeteors = 0;
+        meteorTask = Bukkit.getScheduler().runTaskTimer(RaidCraft.getComponent(SkillsPlugin.class), new Runnable() {
+            @Override
+            public void run() {
 
-        for (int i = 0; i < getAmount(); i++) {
-            origin = origin.add(MathUtil.RANDOM.nextInt(3), MathUtil.RANDOM.nextInt(5) + 3, MathUtil.RANDOM.nextInt(3));
-            RangedAttack<ProjectileCallback> attack = new RangedAttack<>(getHolder(), ProjectileType.LARGE_FIREBALL, getTotalDamage());
-            org.bukkit.entity.Fireball projectile = location.getWorld().spawn(origin, org.bukkit.entity.Fireball.class);
-            projectile.setShooter(getHolder().getPlayer());
-            projectile.setIsIncendiary(true);
-            projectile.setFireTicks(100);
-            attack.setProjectile(projectile);
-
-            Vector direction = new Vector(location.getX() - origin.getX(), location.getY() - origin.getY(), location.getZ() - origin.getZ());
-            direction = direction.normalize().multiply(MathUtil.RANDOM.nextInt(i+1));
-            attack.setVelocity(direction);
-            attack.run();
-        }
+                if (firedMeteors >= amount) {
+                    // cancel the task
+                    meteorTask.cancel();
+                    return;
+                }
+                try {
+                    origin.add(MathUtil.RANDOM.nextInt(3), MathUtil.RANDOM.nextInt(5) + 3, MathUtil.RANDOM.nextInt(3));
+                    RangedAttack<ProjectileCallback> attack = new RangedAttack<>(getHolder(), ProjectileType.LARGE_FIREBALL, getTotalDamage());
+                    LargeFireball projectile = location.getWorld().spawn(origin, LargeFireball.class);
+                    projectile.setShooter(getHolder().getPlayer());
+                    projectile.setIsIncendiary(true);
+                    projectile.setFireTicks(100);
+                    attack.setProjectile(projectile);
+                    if (amount > 1) {
+                        double speedmult = MathUtil.RANDOM.nextDouble() * (maxSpeed - minSpeed) + minSpeed;
+                        // lets randomize the meteor a little if multiple are spawned
+                        direction.multiply(speedmult);
+                    }
+                    attack.setVelocity(direction);
+                    firedMeteors++;
+                    attack.run();
+                } catch (CombatException e) {
+                    getHolder().sendMessage(ChatColor.RED + e.getMessage());
+                }
+            }
+        }, delay, interval);
     }
 }
