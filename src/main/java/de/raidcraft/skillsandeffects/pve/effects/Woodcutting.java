15a4ec9ae045c94691b9c88b645c983d25c74417
IDragonfire <bastian616-git@yahoo.de>
2013-08-19 23:33:05 +0200

    rename AxtProfi to Woodcutting and add TODO's

--- src/main/java/de/raidcraft/skillsandeffects/pve/effects/Woodcutting.java
+++ src/main/java/de/raidcraft/skillsandeffects/pve/effects/Woodcutting.java
@@ -7,15 +7,18 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import com.sk89q.worldedit.blocks.BlockID;
+
 import de.raidcraft.RaidCraft;
 import de.raidcraft.skills.api.combat.EffectType;
+import de.raidcraft.skills.api.combat.action.SkillAction;
 import de.raidcraft.skills.api.effect.EffectInformation;
 import de.raidcraft.skills.api.hero.Hero;
 import de.raidcraft.skills.api.persistance.SkillProperties;
 import de.raidcraft.skills.api.profession.Profession;
 import de.raidcraft.skills.api.skill.AbstractLevelableSkill;
 import de.raidcraft.skills.api.trigger.TriggerHandler;
-import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
 import de.raidcraft.skills.tables.THeroSkill;
 import de.raidcraft.skills.trigger.PlayerInteractTrigger;
 import de.raidcraft.skills.util.ConfigUtil;
@@ -23,13 +26,12 @@ import de.raidcraft.skills.util.ConfigUtil;
 /**
  * @author Silthus
  */
-@EffectInformation(name = "Axt Profi", description = "Fällt direkt einen Baum.", types = { EffectType.HELPFUL })
-public class AxtProfi extends AbstractLevelableSkill {
+@EffectInformation(name = "Woodcutting", description = "Fällt direkt einen Baum.", types = { EffectType.HELPFUL })
+public class Woodcutting extends AbstractLevelableSkill implements Triggered {
 	private ConfigurationSection maxBlockAmountConfig;
 	private double maxBlockAmount;
 
-	public AxtProfi(Hero hero, SkillProperties data, Profession profession,
-			THeroSkill database) {
+	public Woodcutting(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 		super(hero, data, profession, database);
 	}
 
@@ -40,52 +42,48 @@ public class AxtProfi extends AbstractLevelableSkill {
 
 	private int getMaxBlockAmount() {
 		if (maxBlockAmount == 0.0) {
-			maxBlockAmount = ConfigUtil.getTotalValue(this,
-					maxBlockAmountConfig);
+			maxBlockAmount = ConfigUtil.getTotalValue(this, maxBlockAmountConfig);
 		}
 		return (int) maxBlockAmount;
 	}
 
-	private boolean isTreeMaterial(Material mat) {
-		return mat == Material.WOOD;
+	private boolean isTreeMaterial(int mat) {
+		return mat == BlockID.WOOD;
 	}
 
-	private int walkInDirection(PlayerInteractTrigger trigger,
-			BlockFace direction, Block startBlock, int amount, int maxAmount) {
+	private int walkInDirection(PlayerInteractTrigger trigger, BlockFace direction, Block startBlock, int amount) {
 		BlockBreakEvent event;
-		while (amount <= maxAmount && isTreeMaterial(startBlock.getType())) {
+		while (amount <= getMaxBlockAmount() && isTreeMaterial(startBlock.getTypeId())) {
 			if (!RaidCraft.isPlayerPlacedBlock(startBlock)) {
-				// check if blocked
-				event = new BlockBreakEvent(startBlock, trigger.getEvent()
-						.getPlayer());
-				RaidCraft.callEvent(event);
-				if (event.isCancelled()) {
-					startBlock.breakNaturally(trigger.getEvent().getPlayer()
-							.getItemInHand());
-					amount++;
-				}
+
 			}
-			startBlock = startBlock.getRelative(BlockFace.DOWN);
+			// check if blocked
+			event = new BlockBreakEvent(startBlock, trigger.getEvent().getPlayer());
+			RaidCraft.callEvent(event);
+			if (event.isCancelled()) {
+				startBlock.breakNaturally(trigger.getEvent().getPlayer().getItemInHand());
+				amount++;
+			}
+			startBlock = startBlock.getRelative(direction);
 		}
 		return amount;
 	}
 
+	// TODO: holz in der krone abbauen
+	// TODO: use QueuedInteract
 	@TriggerHandler(ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractTrigger trigger) {
 		Block targetBlock = trigger.getEvent().getClickedBlock();
-		if (!isTreeMaterial(targetBlock.getType())) {
+		if (!isTreeMaterial(targetBlock.getTypeId())) {
 			return;
 		}
 		int amount = 0;
-		int maxAmount = getMaxBlockAmount();
 		// first walk down
-		amount = walkInDirection(trigger, BlockFace.DOWN, targetBlock, amount,
-				maxAmount);
+		amount = walkInDirection(trigger, BlockFace.DOWN, targetBlock, amount);
 		// then walk up
-		amount = walkInDirection(trigger, BlockFace.UP, targetBlock, amount,
-				maxAmount);
+		amount = walkInDirection(trigger, BlockFace.UP, targetBlock, amount);
 		getHolder().sendMessage(
-				ChatColor.YELLOW + "Du hast direkt " + ChatColor.RED + amount
-						+ ChatColor.YELLOW + " Holz abgebaut.");
+				ChatColor.YELLOW + "Du hast direkt " + ChatColor.RED + amount + ChatColor.YELLOW + " Holz abgebaut.");
+		substractUsageCost(new SkillAction(this));
 	}
 }
