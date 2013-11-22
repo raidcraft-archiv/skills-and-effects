85d42c6545acad8d40ad71482db2dcda087b62f0
Silthus <md.doering@yahoo.de>
2013-11-22 21:58:25 +0100

    aura is now an on off skill

--- src/main/java/de/raidcraft/skillsandeffects/pvp/effects/buffs/aura/AbstractAura.java
+++ src/main/java/de/raidcraft/skillsandeffects/pvp/effects/buffs/aura/AbstractAura.java
@@ -1,7 +1,7 @@
 package de.raidcraft.skillsandeffects.pvp.effects.buffs.aura;
 
 import de.raidcraft.skills.api.character.CharacterTemplate;
-import de.raidcraft.skills.api.effect.types.ExpirableEffect;
+import de.raidcraft.skills.api.effect.AbstractEffect;
 import de.raidcraft.skills.api.effect.IgnoredEffect;
 import de.raidcraft.skills.api.exceptions.CombatException;
 import de.raidcraft.skills.api.persistance.EffectData;
@@ -11,7 +11,7 @@ import de.raidcraft.skillsandeffects.pvp.skills.buffs.Aura;
  * @author Silthus
  */
 @IgnoredEffect
-public abstract class AbstractAura extends ExpirableEffect<Aura> {
+public abstract class AbstractAura extends AbstractEffect<Aura> {
 
     public AbstractAura(Aura source, CharacterTemplate target, EffectData data) {
 
