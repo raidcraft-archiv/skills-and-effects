5598fe0050840a76480eb6d4cddc8a3ced73b84a
Silthus <silthus@raid-craft.de>
2014-02-22 11:08:21 +0100

    added the recursive block break skill and effect adds #469 and fixes #697

---
+++ src/main/java/de/raidcraft/skillsandeffects/pve/effects/tools/RecursiveBlockBreakEffect.java
@@ -0,0 +1,98 @@
+package de.raidcraft.skillsandeffects.pve.effects.tools;
+
+import de.raidcraft.RaidCraft;
+import de.raidcraft.api.language.Translator;
+import de.raidcraft.skills.SkillsPlugin;
+import de.raidcraft.skills.api.character.CharacterTemplate;
+import de.raidcraft.skills.api.effect.EffectInformation;
+import de.raidcraft.skills.api.effect.types.ExpirableEffect;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.persistance.EffectData;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.trigger.PlayerInteractTrigger;
+import de.raidcraft.skillsandeffects.pve.skills.RecursiveBlockBreak;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Philip
+ */
+@EffectInformation(
+        name = "Recursive Block Break",
+        description = "Breaks the defined amount of blocks recursivly."
+)
+public class RecursiveBlockBreakEffect extends ExpirableEffect<RecursiveBlockBreak> implements Triggered {
+
+    public RecursiveBlockBreakEffect(RecursiveBlockBreak source, CharacterTemplate target, EffectData data) {
+
+        super(source, target, data);
+        // hard set the duration to 5s
+        setDuration(5.0);
+    }
+
+    @TriggerHandler(ignoreCancelled = true)
+    public void onInteract(PlayerInteractTrigger trigger) throws CombatException {
+
+        PlayerInteractEvent event = trigger.getEvent();
+
+        if (event.getAction() != Action.LEFT_CLICK_BLOCK) {
+            return;
+        }
+
+        // check if correct tool in use
+        if (event.getItem() == null || event.getItem().isSimilar(getSource().getTool())) {
+            warn(Translator.tr(SkillsPlugin.class, event.getPlayer(),
+                    "skills.recursive-block-break.wrong-tool", "You have the wrong tool equiped for this skill!"));
+            return;
+        }
+
+        int amount = breakRecursive(event.getPlayer(), getSource().getTool(), event.getClickedBlock(), 0, getSource().getMaxAmount());
+        info(Translator.tr(SkillsPlugin.class, event.getPlayer(),
+                "skills.recursive-block-break.break-amount", "You have destroyed {0} blocks with {1}.", amount, getFriendlyName()));
+        remove();
+    }
+
+    public int breakRecursive(Player player, ItemStack tool, Block block, int currentAmount, int maxAmount) {
+
+        if (currentAmount < maxAmount && getSource().getAllowedBlocks().contains(block.getType())) {
+            BlockBreakEvent event = new BlockBreakEvent(block, player);
+            RaidCraft.callEvent(event);
+            if (event.isCancelled()) {
+                return currentAmount;
+            }
+            block.breakNaturally(tool);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.DOWN), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.NORTH), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.NORTH_EAST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.EAST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.SOUTH_EAST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.SOUTH), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.SOUTH_WEST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.WEST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.NORTH_WEST), currentAmount + 1, maxAmount);
+            currentAmount = breakRecursive(player, tool, block.getRelative(BlockFace.UP), currentAmount + 1, maxAmount);
+        }
+        return currentAmount;
+    }
+
+    @Override
+    protected void apply(CharacterTemplate target) throws CombatException {
+
+    }
+
+    @Override
+    protected void remove(CharacterTemplate target) throws CombatException {
+
+    }
+
+    @Override
+    protected void renew(CharacterTemplate target) throws CombatException {
+
+    }
+}
---
+++ src/main/java/de/raidcraft/skillsandeffects/pve/skills/RecursiveBlockBreak.java
@@ -0,0 +1,109 @@
+package de.raidcraft.skillsandeffects.pve.skills;
+
+import de.raidcraft.RaidCraft;
+import de.raidcraft.api.items.CustomItemException;
+import de.raidcraft.skills.api.combat.action.SkillAction;
+import de.raidcraft.skills.api.combat.callback.Callback;
+import de.raidcraft.skills.api.exceptions.CombatException;
+import de.raidcraft.skills.api.hero.Hero;
+import de.raidcraft.skills.api.persistance.SkillProperties;
+import de.raidcraft.skills.api.profession.Profession;
+import de.raidcraft.skills.api.skill.AbstractSkill;
+import de.raidcraft.skills.api.skill.SkillInformation;
+import de.raidcraft.skills.api.trigger.TriggerHandler;
+import de.raidcraft.skills.api.trigger.TriggerPriority;
+import de.raidcraft.skills.api.trigger.Triggered;
+import de.raidcraft.skills.tables.THeroSkill;
+import de.raidcraft.skills.trigger.PlayerInteractTrigger;
+import de.raidcraft.skills.util.ConfigUtil;
+import de.raidcraft.skillsandeffects.pve.effects.tools.SpeedBlockBreakEffect;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Silthus
+ */
+@SkillInformation(
+        name = "Recursive Block Break",
+        description = "Breaks blocks recursivly.",
+        queuedAttack = true
+)
+public class RecursiveBlockBreak extends AbstractSkill implements Triggered {
+
+    private final Set<Material> allowedBlocks = new HashSet<>();
+    private ItemStack tool;
+    private int maxAmount;
+
+    public RecursiveBlockBreak(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
+
+        super(hero, data, profession, database);
+    }
+
+    @Override
+    public void load(ConfigurationSection data) {
+
+        try {
+            this.tool = RaidCraft.getItem(data.getString("tool"));
+            this.maxAmount = (int) ConfigUtil.getTotalValue(this, data.getConfigurationSection("max-amount"));
+            for (String entry : data.getStringList("allowed-blocks")) {
+                Material material = Material.matchMaterial(entry);
+                if (material != null) {
+                    allowedBlocks.add(material);
+                } else {
+                    RaidCraft.LOGGER.warning("Wrong material in config of " + getName() + ": " + entry);
+                }
+            }
+        } catch (CustomItemException e) {
+            warn(e);
+        }
+    }
+
+    public Set<Material> getAllowedBlocks() {
+
+        return allowedBlocks;
+    }
+
+    public ItemStack getTool() {
+
+        return tool;
+    }
+
+    public int getMaxAmount() {
+
+        return maxAmount;
+    }
+
+    @TriggerHandler(ignoreCancelled = true, priority = TriggerPriority.MONITOR)
+    public void onPlayerInteract(PlayerInteractTrigger trigger) throws CombatException {
+
+        if (trigger.getEvent().getAction() != Action.RIGHT_CLICK_BLOCK || !isValid(trigger)) {
+            return;
+        }
+        final SkillAction skillAction = new SkillAction(this);
+        checkUsage(skillAction);
+
+        queueInteract(new Callback<PlayerInteractTrigger>() {
+            @Override
+            public void run(PlayerInteractTrigger trigger) throws CombatException {
+
+                if (trigger.getEvent().getAction() != Action.LEFT_CLICK_BLOCK || !isValid(trigger)) {
+                    return;
+                }
+
+                addEffect(getHolder(), SpeedBlockBreakEffect.class);
+                substractUsageCost(skillAction);
+            }
+        }, Action.LEFT_CLICK_BLOCK);
+    }
+
+    public boolean isValid(PlayerInteractTrigger trigger) {
+
+        return allowedBlocks.contains(trigger.getEvent().getClickedBlock().getType())
+                && trigger.getEvent().getItem() != null && tool.isSimilar(trigger.getEvent().getItem());
+    }
+}
