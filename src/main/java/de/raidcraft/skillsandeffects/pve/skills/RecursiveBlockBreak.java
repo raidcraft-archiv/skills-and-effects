4464028114dd3005ebf239707e6e93e727cc0440
Silthus <m.silthus@gmail.com>
2015-05-24 18:31:47 +0200

    use mcmmo woodcutting skill #1433

--- src/main/java/de/raidcraft/skillsandeffects/pve/skills/RecursiveBlockBreak.java
+++ src/main/java/de/raidcraft/skillsandeffects/pve/skills/RecursiveBlockBreak.java
@@ -1,11 +1,5 @@
 package de.raidcraft.skillsandeffects.pve.skills;
 
-import com.sk89q.worldedit.MaxChangedBlocksException;
-import com.sk89q.worldedit.PlayerDirection;
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.blocks.BlockID;
-import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.world.World;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.skills.api.combat.action.SkillAction;
 import de.raidcraft.skills.api.effect.common.QueuedInteract;
@@ -24,12 +18,18 @@ import de.raidcraft.skills.util.ConfigUtil;
 import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Tree;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -42,14 +42,13 @@ import java.util.Set;
 )
 public class RecursiveBlockBreak extends AbstractSkill implements Triggered {
 
-    private static final int RANGE_SQUARED = 30 * 30;
-
     @Getter
     private final Set<Material> allowedTools = new HashSet<>();
     @Getter
     private final Set<Material> allowedBlocks = new HashSet<>();
     @Getter
     private int maxAmount;
+    private boolean treeFellerReachedThreshold = false;
 
     public RecursiveBlockBreak(Hero hero, SkillProperties data, Profession profession, THeroSkill database) {
 
@@ -96,22 +95,7 @@ public class RecursiveBlockBreak extends AbstractSkill implements Triggered {
             }
 
             substractUsageCost(new SkillAction(RecursiveBlockBreak.this));
-            Block block = callback.getEvent().getClickedBlock();
-            int amount = 0;
-            int maxAmount = getMaxAmount();
-            try {
-                org.bukkit.World world = event.getPlayer().getWorld();
-                Set<Vector> blocks = bfs(new BukkitWorld(world), new Vector(block.getX(), block.getY(), block.getZ()));
-                for (Vector vector : blocks) {
-                    if (amount >= maxAmount) break;
-                    Block blockAt = world.getBlockAt(vector.getBlockX(), vector.getBlockY(), vector.getBlockZ());
-                    blockAt.breakNaturally(callback.getEvent().getItem());
-                    amount++;
-                }
-                info("Du hast " + amount + "/" + maxAmount + " Blöcke zerstört.");
-            } catch (MaxChangedBlocksException e) {
-                warn(e.getMessage());
-            }
+            processTreeFeller(callback.getEvent().getClickedBlock().getState());
         }, Action.LEFT_CLICK_BLOCK);
     }
 
@@ -127,66 +111,213 @@ public class RecursiveBlockBreak extends AbstractSkill implements Triggered {
                 && allowedBlocks.contains(event.getClickedBlock().getType());
     }
 
-    Vector[] recurseDirections = {
-            PlayerDirection.NORTH.vector(),
-            PlayerDirection.EAST.vector(),
-            PlayerDirection.SOUTH.vector(),
-            PlayerDirection.WEST.vector(),
-            PlayerDirection.UP.vector(),
-            PlayerDirection.DOWN.vector(),
+    /**
+     * Begins Tree Feller
+     *
+     * @param blockState Block being broken
+     */
+    public void processTreeFeller(BlockState blockState) {
+
+        Player player = getHolder().getPlayer();
+        Set<BlockState> treeFellerBlocks = new HashSet<BlockState>();
+
+        treeFellerReachedThreshold = false;
+
+        processTree(blockState, treeFellerBlocks);
+
+        // If the player is trying to break too many blocks
+        if (treeFellerReachedThreshold) {
+            treeFellerReachedThreshold = false;
+
+            warn("Zu viele Blöcke!");
+            return;
+        }
+
+        dropBlocks(treeFellerBlocks);
+        treeFellerReachedThreshold = false; // Reset the value after we're done with Tree Feller each time.
+    }
+
+    /**
+     * Handles the dropping of blocks
+     *
+     * @param treeFellerBlocks List of blocks to be dropped
+     */
+    private void dropBlocks(Set<BlockState> treeFellerBlocks) {
+
+        Player player = getHolder().getPlayer();
+        int xp = 0;
+
+        for (BlockState blockState : treeFellerBlocks) {
+            Block block = blockState.getBlock();
+
+            BlockBreakEvent event = new BlockBreakEvent(block, player);
+            RaidCraft.callEvent(event);
+            if (event.isCancelled()) {
+                continue;
+            }
+
+            Material material = blockState.getType();
+
+            if (blockState.getData() instanceof Tree) {
+                Tree tree = (Tree) blockState.getData();
+                tree.setDirection(BlockFace.UP);
+            }
+
+            switch (material) {
+                case LOG:
+                case LOG_2:
+                    block.breakNaturally(player.getItemInHand());
+                    break;
+            }
+
+            blockState.setType(Material.AIR);
+            blockState.update(true);
+        }
+    }
+
+    /**
+     * The x/y differences to the blocks in a flat cylinder around the center
+     * block, which is excluded.
+     */
+    private static final int[][] directions = {
+            new int[]{-2, -1}, new int[]{-2, 0}, new int[]{-2, 1},
+            new int[]{-1, -2}, new int[]{-1, -1}, new int[]{-1, 0}, new int[]{-1, 1}, new int[]{-1, 2},
+            new int[]{0, -2}, new int[]{0, -1}, new int[]{0, 1}, new int[]{0, 2},
+            new int[]{1, -2}, new int[]{1, -1}, new int[]{1, 0}, new int[]{1, 1}, new int[]{1, 2},
+            new int[]{2, -1}, new int[]{2, 0}, new int[]{2, 1},
     };
 
     /**
-     * Helper method.
+     * Processes Tree Feller in a recursive manner
      *
-     * @param world  the world that contains the tree
-     * @param origin any point contained in the floating tree
-     *
-     * @return a set containing all blocks in the tree/shroom or null if this is not a floating tree/shroom.
+     * @param blockState Block being checked
+     * @param treeFellerBlocks List of blocks to be removed
      */
-    private Set<Vector> bfs(World world, Vector origin) throws MaxChangedBlocksException {
+    /*
+     * Algorithm: An int[][] of X/Z directions is created on static class
+     * initialization, representing a cylinder with radius of about 2 - the
+     * (0,0) center and all (+-2, +-2) corners are omitted.
+     *
+     * handleBlock() returns a boolean, which is used for the sole purpose of
+     * switching between these two behaviors:
+     *
+     * (Call blockState "this log" for the below explanation.)
+     *
+     *  [A] There is another log above this log (TRUNK)
+     *    Only the flat cylinder in the directions array is searched.
+     *  [B] There is not another log above this log (BRANCH AND TOP)
+     *    The cylinder in the directions array is extended up and down by 1
+     *    block in the Y-axis, and the block below this log is checked as
+     *    well. Due to the fact that the directions array will catch all
+     *    blocks on a red mushroom, the special method for it is eliminated.
+     *
+     * This algorithm has been shown to achieve a performance of 2-5
+     * milliseconds on regular trees and 10-15 milliseconds on jungle trees
+     * once the JIT has optimized the function (use the ability about 4 times
+     * before taking measurements).
+     */
+    protected void processTree(BlockState blockState, Set<BlockState> treeFellerBlocks) {
 
-        final Set<Vector> visited = new HashSet<Vector>();
-        final LinkedList<Vector> queue = new LinkedList<Vector>();
+        List<BlockState> futureCenterBlocks = new ArrayList<BlockState>();
 
-        queue.addLast(origin);
-        visited.add(origin);
+        // Check the block up and take different behavior (smaller search) if it's a log
+        if (handleBlock(blockState.getBlock().getRelative(BlockFace.UP).getState(), futureCenterBlocks, treeFellerBlocks)) {
+            for (int[] dir : directions) {
+                handleBlock(blockState.getBlock().getRelative(dir[0], 0, dir[1]).getState(), futureCenterBlocks, treeFellerBlocks);
 
-        while (!queue.isEmpty()) {
-            final Vector current = queue.removeFirst();
-            for (Vector recurseDirection : recurseDirections) {
-                final Vector next = current.add(recurseDirection);
-                if (origin.distanceSq(next) > RANGE_SQUARED) {
-                    // Maximum range exceeded => stop walking
-                    continue;
+                if (treeFellerReachedThreshold) {
+                    return;
                 }
+            }
+        } else {
+            // Cover DOWN
+            handleBlock(blockState.getBlock().getRelative(BlockFace.DOWN).getState(), futureCenterBlocks, treeFellerBlocks);
+            // Search in a cube
+            for (int y = -1; y <= 1; y++) {
+                for (int[] dir : directions) {
+                    handleBlock(blockState.getBlock().getRelative(dir[0], y, dir[1]).getState(), futureCenterBlocks, treeFellerBlocks);
 
-                if (visited.add(next)) {
-                    switch (world.getBlockType(next)) {
-                        case BlockID.AIR:
-                        case BlockID.SNOW:
-                            // we hit air or snow => stop walking this route
-                            continue;
+                    if (treeFellerReachedThreshold) {
+                        return;
+                    }
+                }
+            }
+        }
 
-                        case BlockID.LOG:
-                        case BlockID.LOG2:
-                        case BlockID.LEAVES:
-                        case BlockID.LEAVES2:
-                        case BlockID.BROWN_MUSHROOM_CAP:
-                        case BlockID.RED_MUSHROOM_CAP:
-                        case BlockID.VINE:
-                            // queue next point
-                            queue.addLast(next);
-                            break;
+        // Recursive call for each log found
+        for (BlockState futureCenterBlock : futureCenterBlocks) {
+            if (treeFellerReachedThreshold) {
+                return;
+            }
 
-                        default:
-                            // we hit something solid => stop walking this route
-                            continue;
-                    } // switch
-                } // if
-            } // for
-        } // while
+            processTree(futureCenterBlock, treeFellerBlocks);
+        }
+    }
 
-        return visited;
-    } // bfs
+    /**
+     * Handle a block addition to the list of blocks to be removed and to the
+     * list of blocks used for future recursive calls of
+     * 'processTree()'
+     *
+     * @param blockState         Block to be added
+     * @param futureCenterBlocks List of blocks that will be used to call
+     *                           'processTree()'
+     * @param treeFellerBlocks   List of blocks to be removed
+     *
+     * @return true if and only if the given blockState was a Log not already
+     * in treeFellerBlocks.
+     */
+    private boolean handleBlock(BlockState blockState, List<BlockState> futureCenterBlocks, Set<BlockState> treeFellerBlocks) {
+
+        // Without this check Tree Feller propagates through leaves until the threshold is hit
+        if (treeFellerBlocks.size() > getMaxAmount()) {
+            treeFellerReachedThreshold = true;
+        }
+
+        if (isLog(blockState)) {
+            treeFellerBlocks.add(blockState);
+            futureCenterBlocks.add(blockState);
+            return true;
+        } else if (isLeaves(blockState)) {
+            treeFellerBlocks.add(blockState);
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Check if a given block is a log
+     *
+     * @param blockState The {@link BlockState} of the block to check
+     *
+     * @return true if the block is a log, false otherwise
+     */
+    public boolean isLog(BlockState blockState) {
+
+        switch (blockState.getType()) {
+            case LOG:
+            case LOG_2:
+            case HUGE_MUSHROOM_1:
+            case HUGE_MUSHROOM_2:
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if a given block is a leaf
+     *
+     * @param blockState The {@link BlockState} of the block to check
+     *
+     * @return true if the block is a leaf, false otherwise
+     */
+    public boolean isLeaves(BlockState blockState) {
+
+        switch (blockState.getType()) {
+            case LEAVES:
+            case LEAVES_2:
+                return true;
+        }
+        return false;
+    }
 }
